0a8079d4b1ff068374f2a3f71d2aad39c8082d8c
AngelOrts <angel.orts.manrique@everis.com>
2018-12-11 09:20:40 +0100

    - Added style in hero details component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {MenubarModule} from 'primeng/menubar';
 import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/primeng';
 
 @NgModule({
   /* CommonModule --> Exports all the basic Angular directives and pipes, such as NgIf, NgForOf, DecimalPipe, and so on.
@@ -10,7 +11,7 @@ import {ButtonModule} from 'primeng/button';
    when you create a new app with the CLI new command. */
   /* FormsModule --> Exports the required providers and directives for template-driven forms,
   making them available for import by NgModules that import this module.*/
-  imports: [MenubarModule, ButtonModule],
+  imports: [MenubarModule, ButtonModule, InputTextModule],
   exports: [CommonModule, FormsModule, MenubarModule, ButtonModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
